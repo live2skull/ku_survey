cc014bb8ec794ca3f7b54f0dd58bd871a657a691
zrkrbit <zrkrbit@gmail.com>
2017-03-07 18:35:45 +0900

    navbar fix (모바일)

--- public/javascripts/controller/navbar.js
+++ public/javascripts/controller/navbar.js
@@ -3,6 +3,9 @@ angular.module('kudoc')
 .controller('navbarCtrl', function ($scope, sessionFactory) {
 
     $scope.click = {};
+    function checkValidateShow() {
+        return $(window).width() > 480;
+    };
     $scope.checkURL = function (chk)
     {
         var url = location.href;
@@ -11,6 +14,10 @@ angular.module('kudoc')
 
         return url.indexOf(chk) != -1
     };
+    $scope.resolveTitle = function () {
+        if (checkValidateShow()) return "고려대학교 세종캠퍼스 온라인 학생설문시스템";
+        else return "고려대학교 온라인 학생설문시스템"
+    }
 
     function callback_logout () {
         location.href = '/';
