dbe6fdf0eb3e3945949fcd6fae69e0e540bab0df
live2skull <live2skull@gmail.com>
2017-04-17 17:07:29 +0900

    더이상 사용자에게 오류를 보여주지 않음

--- app.js
+++ app.js
@@ -97,8 +97,11 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('_default/error.jade');
+    // 서비스 정보가 노출되므로, 임시로 설정하도록 함.
+    res.status(403);
+    res.send("Internal Server Error");
+  // res.status(err.status || 500);
+  // res.render('_default/error.jade');
 });
 
 mysql.pool.getConnection(function (err, conn) {
