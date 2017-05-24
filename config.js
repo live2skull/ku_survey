2e6d4599035e44f8c58b6193eed8b802ae46f131
live2skull <live2skull@gmail.com>
2017-05-24 20:53:20 +0900

    Internet Explorer 에서 input의 height가 작아 글씨가 보이지 않던 문제 수정.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ exports.EXPOSE_SSODEBUG = false;
 exports.SSO_UNSECURE = false;
 
 
-exports.REDIS_SERVER = '192.168.1.10';
+exports.REDIS_SERVER = '192.168.1.23';
 exports.REDIS_PORT = 6379;
 
 // exports.SSO_HOST = "163.152.215.187";
